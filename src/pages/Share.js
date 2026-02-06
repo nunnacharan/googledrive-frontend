57bd71daed07f314bea859a864b3802cc8c47578
Nunna Charan Naga Lakshmi Narayana <109160426+nunnacharan@users.noreply.github.com>
2026-02-06 21:01:44 +0530

    Update Share.js

--- src/pages/Share.js
+++ src/pages/Share.js
@@ -5,7 +5,8 @@ export default function Share() {
   const { token } = useParams();
 
   useEffect(() => {
-    window.location.href = `${process.env.REACT_APP_API_URL}/files/public/${token}`;
+    const baseUrl = process.env.REACT_APP_API_URL.replace(/\/+$/, "");
+    window.location.href = `${baseUrl}/files/public/${token}`;
   }, [token]);
 
   return <p className="text-center mt-20">Opening shared file...</p>;
